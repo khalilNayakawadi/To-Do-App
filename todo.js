593ac13de5d9d713276a4637694f15a1d424204c
khalilNayakawadi <136471783+khalilNayakawadi@users.noreply.github.com>
2024-09-06 19:43:38 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,24 +3,29 @@ let addTaskBtn = document.getElementById("addtask") ;
 tasks = document.getElementById("tasks");
 let task = document.getElementsByClassName("taskdiv");
 
+let taskDiv = document.createElement('div');
+let taskName = document.createElement('h4')
+let taskDone = document.createElement('input')
+let removerBtn = document.createElement('button')
 
 addTaskBtn.addEventListener('click',()=>{
     if(inputTask.value != ""){
-        let task = document.createElement('div');
-        let taskName = document.createElement('h4')
-        let taskDone = document.createElement('input')
-        let removerBtn = document.createElement('button')
+        taskDiv = document.createElement('div');
+        taskName = document.createElement('h4')
+        taskDone = document.createElement('input')
+        removerBtn = document.createElement('button')
         taskDone.type = "checkbox";
+        taskDone.style.width = "30px";
+        taskDone.style.height= "30px";
         taskName.innerText = inputTask.value ;
-        task.classList.add("taskdiv")
+        taskDiv.classList.add("taskdiv")
         removerBtn.classList.add("btn")
         removerBtn.classList.add("btn-outline-primary")
         removerBtn.innerText = "🗑️"
-        tasks.appendChild(task)
-        task.appendChild(taskName)
-        task.appendChild(taskDone)
-        task.appendChild(removerBtn)
+        tasks.appendChild(taskDiv)
+        taskDiv.appendChild(taskName)
+        taskDiv.appendChild(taskDone)
+        
         inputTask.value = "";
     }
- 
 })
